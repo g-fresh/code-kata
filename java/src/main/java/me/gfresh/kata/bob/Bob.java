a8f939e11dbdf45a1a4ea84c738428b0b4ccc144
jfischer <jfischer@devbox>
2015-11-24 21:03:57 +0100

    Exercism kata 'Bob'

---
+++ java/src/main/java/me/gfresh/kata/bob/Bob.java
@@ -0,0 +1,31 @@
+package me.gfresh.kata.bob;
+
+public final class Bob {
+
+	public String hey(String message) {
+		if (wasShouted(message)) return "Whoa, chill out!";
+		if (isQuestion(message)) return "Sure.";
+		if (dontUnderstand(message)) return "Fine. Be that way!";
+		return "Whatever.";
+	}
+
+	private boolean wasShouted(String message) {
+		return containsLetter(message) && allUpperCase(message);
+	}
+
+	private boolean containsLetter(String message) {
+		return message.chars().anyMatch(Character::isLetter);
+	}
+
+	private boolean allUpperCase(String message) {
+		return ! message.chars().anyMatch(Character::isLowerCase);
+	}
+
+	private boolean isQuestion(String message) {
+		return message.endsWith("?");
+	}
+
+	private boolean dontUnderstand(String message) {
+		return message.trim().isEmpty();
+	}
+}
