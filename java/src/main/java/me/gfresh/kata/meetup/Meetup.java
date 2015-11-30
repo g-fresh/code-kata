86e37e284f075f73a745890786e019d64beaa31d
Joerg Fischer <j.o.e.r.g@gmx.net>
2015-11-30 23:12:00 +0100

    Exercism kata 'Meetup'

---
+++ java/src/main/java/me/gfresh/kata/meetup/Meetup.java
@@ -0,0 +1,18 @@
+package me.gfresh.kata.meetup;
+
+import org.joda.time.*;
+
+public final class Meetup {
+
+	private int month;
+	private int year;
+
+	public Meetup(int month, int year) {
+		this.month = month;
+		this.year = year;
+	}
+
+	public DateTime day(int dayOfWeek, MeetupSchedule schedule) {
+		return schedule.calculate(month, year, dayOfWeek);
+	}
+}
